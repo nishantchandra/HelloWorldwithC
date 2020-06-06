1aa0169a5dbdb9ffb3835270cca9dbb02a4c0de2
Nishant Chandra <nishantchandra@Nishants-Air.attlocal.net>
2020-06-06 14:54:07 -0700

    Adding in files

---
+++ HelloWorld.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int squared(int x) {
+	return x * x;
+}
+
+int hex(){
+	for (int i = 0; i < 16; ++i)
+	{
+		printf("%x \n",i);
+
+	}
+
+	return 0;
+
+
+}
+
+
+
+int main(void) {
+ 	
+	printf("Hellooooo World!\n");
+
+	printf("GoooodBye!\n");
+
+	printf("Please enter first number");
+
+	int num = 0;
+	int test = 0;
+	scanf("%d",&num);
+
+	printf("Please enter second number");
+
+	scanf("%d",&test);
+
+	
+	printf("the answer is %d and the answer is %d", squared(num), squared(test));
+	printf ("\n");
+
+	hex();	
+
+	//printf("hex %x", 10);
+	//printf ("\n"); 
+
+	return 0;
+
+} 
+
+
+
